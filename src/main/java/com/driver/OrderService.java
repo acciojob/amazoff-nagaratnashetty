d1a903210bbe864d58a387ba4e1ecd9641877e7f
nagaratnashetty <125356022+nagaratnashetty@users.noreply.github.com>
2024-04-03 22:14:47 +0530

    Update OrderService.java

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -1,6 +1,5 @@
 package com.driver;
 
-import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,97 +7,69 @@ import java.util.List;
 
 @Service
 public class OrderService {
-
-    OrderRepository orderRepository=new OrderRepository();
-
-
-    public void addOrder(Order order){
+    //    @Autowired
+    OrderRepository orderRepository = new OrderRepository();
+    public void addOrder(Order order) {
         orderRepository.addOrder(order);
     }
 
-    public void addPartner(String partnerId){
+    public void addPartner(String partnerId) {
         orderRepository.addPartner(partnerId);
     }
 
-
-    public void addOrderPartnerPair(String orderId, String partnerId){
+    public void addOrderPartnerPair(String orderId, String partnerId) {
         orderRepository.addOrderPartnerPair(orderId, partnerId);
     }
 
-    public Order getOrderById(String orderId){
-        Order order = orderRepository.getOrderById(orderId);
-        return order;
+    public Order getOrderById(String orderId) {
+        return orderRepository.getOrderById(orderId);
     }
 
-    public DeliveryPartner getPartnerById(String partnerId){
-        DeliveryPartner partner = orderRepository.getPartnerById(partnerId);
-        return partner;
+    public DeliveryPartner getPartnerById(String partnerId) {
+        return orderRepository.getPartnerById(partnerId);
     }
 
-    public int getOrderCountByPartnerId(String partnerId){
-        int orderCount = orderRepository.getOrderCountByPartnerId(partnerId);
-        return orderCount;
+    public Integer getOrderCountByPartnerId(String partnerId) {
+        return orderRepository.getOrderCountByPartnerId(partnerId);
     }
 
-    public List<String> getOrdersByPartnerId(String partnerId){
-        List<String> list = orderRepository.getOrdersByPartnerId(partnerId);
-        return list;
-
+    public List<String> getOrdersByPartnerId(String partnerId) {
+        return orderRepository.getOrdersByPartnerId(partnerId);
     }
 
-    public List<String> getAllOrders(){
-        List<String> list = orderRepository.getAllOrders();
-        return list;
+    public List<String> getAllOrders() {
+        return orderRepository.getAllOrders();
     }
 
-    public int getCountOfUnassignedOrders(){
-        int count = orderRepository.getCountOfUnassignedOrders();
-        return count;
+    public Integer getCountOfUnassignedOrders() {
+        return orderRepository.getCountOfUnassignedOrders();
     }
 
-    public int getOrdersLeftAfterGivenTimeByPartnerId(String time, String partnerId){
-//        String s1 = String.valueOf(time.charAt(0) + time.charAt(1));
-//        String s2 = String.valueOf(time.charAt(3) + time.charAt(4));
-//        int hh = Integer.valueOf(s1);
-//        int mm = Integer.valueOf(s2);
-
-        String arr[] = time.split(":");
-        int hh = Integer.parseInt(arr[0]);
-        int mm = Integer.parseInt(arr[1]);
-
-        int timeInt = (hh*60)+mm;
-
-        int count = orderRepository.getOrdersLeftAfterGivenTimeByPartnerId(timeInt, partnerId);
-
-        return count;
+    public String getLastDeliveryTimeByPartnerId(String partnerId) {
+        int lastDeliveryTime = orderRepository.getLastDeliveryTimeByPartnerId(partnerId);
+        if(lastDeliveryTime == -1)
+            return null;
+        String MM = String.valueOf(lastDeliveryTime % 60);
+        String HH = String.valueOf(lastDeliveryTime / 60);
+        if(HH.length() < 2)
+            HH = "0" + HH;
+        if(MM.length() < 2)
+            MM = "0" + MM;
+        return (HH + ":" + MM);
     }
 
-    public String getLastDeliveryTimeByPartnerId(String partnerId){
-
-        int timeInt = orderRepository.getLastDeliveryTimeByPartnerId(partnerId);
-        int hh = timeInt/60;
-        int mm = timeInt%60;
-        String HH = String.valueOf(hh);
-        if(HH.length()==1){
-            HH = '0' + HH;
-        }
-        String MM = String.valueOf(mm);
-        if(MM.length()==1){
-            MM = '0'+MM;
-        }
-
-        String time = HH + ":" + MM;
-        return time;
+    public Integer getOrdersLeftAfterGivenTimeByPartnerId(String time, String partnerId) {
+        int HH = Integer.parseInt(time.substring(0, 2));
+        int MM = Integer.parseInt(time.substring(3, 5));
+        int givenTime  = (HH * 60) + MM;
+        return orderRepository.getOrdersLeftAfterGivenTimeByPartnerId(givenTime, partnerId);
     }
 
-
-    public void deletePartnerById(String partnerId){
-
+    public void deletePartnerById(String partnerId) {
         orderRepository.deletePartnerById(partnerId);
     }
 
-    public void deleteOrderById(String orderId){
-
+    public void deleteOrderById(String orderId) {
         orderRepository.deleteOrderById(orderId);
     }
 }
